61f04c581ee21196722dbedcf4fbc39d2e0d2b1f
Kora Loudermilk <koraloudermilk@gmail.com>
2019-10-21 12:58:46 -0400

    add module class

---
+++ Bunni/Resources/Modules/Module.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bunni.Resources.Modules
+{
+    public class Module
+    {
+        public Entity Parent { get; set; }
+
+        public Module(Entity _parent)
+        {
+            Parent = _parent;
+        }
+    }
+}
